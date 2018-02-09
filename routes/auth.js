426ebcd8814feb61d6837ca48a61873cce51d043
Pere Amorós <amorospere@gmail.com>
2018-02-09 17:22:19 +0100

    sign up

--- routes/auth.js
+++ routes/auth.js
@@ -1,20 +1,65 @@
 'use strict';
 
 const express = require('express');
-const authRoutes = express.Router();
+const routes = express.Router();
 const User = require('../models/users');
 
-authRoutes.get('/signup', (req, res, next) => {
+// GET SignUp
+routes.get('/signup', (req, res, next) => {
   if (req.session.currentUser) {
     return res.redirect('/');
   }
-  const data = {
-    title: 'Signup'
-  };
-  res.render('auth/signup', data);
+  res.render('auth/signup');
 });
 
-authRoutes.get('/login', (req, res, next) => {
+// POST SignUp
+routes.post('/signup', (req, res, next) => {
+  if (req.session.User) {
+    res.redirect('/');
+  }
+
+  const name = req.body.name;
+  const username = req.body.username;
+  const password = req.body.password;
+
+  // Validate
+  if (username === '' || password === '' || password.length < 8 || !password.match(/[A-Z]/)) {
+    const data = {
+      message: 'All fields are required. Password needs to be 8 characters long and at least one capitalized letter.'
+    };
+    return res.render('auth/signup', data);
+  }
+
+  // check if user with this username already exists
+  User.findOne({ 'username': username }, (err, user) => {
+    if (err) {
+      return next(err);
+    }
+    if (user) {
+      const data = {
+        message: 'The "' + username + '" username is taken'
+      };
+      return res.render('auth/signup', data);
+    }
+    const salt = bcrypt.genSaltSync(bcryptSalt);
+    const hashPass = bcrypt.hashSync(password, salt);
+
+    const newUser = new User({
+      name,
+      username,
+      password: hashPass
+    });
+
+    newUser.save()
+      .then((response) => {
+        req.session.currentUser = newUser;
+        res.redirect('/');
+      })
+      .catch(next);
+  });
+});
+
+routes.get('/login', (req, res, next) => {
   res.render('auth/login');
 });
 
@@ -23,7 +68,7 @@ const bcrypt = require('bcrypt');
 const bcryptSalt = 10;
 
 /* handle the POST from the login form. */
-authRoutes.post('/login', (req, res, next) => {
+routes.post('/login', (req, res, next) => {
   if (req.session.currentUser) {
     return res.redirect('/');
   }
@@ -64,56 +109,9 @@ authRoutes.post('/login', (req, res, next) => {
   });
 });
 
-authRoutes.post('/signup', (req, res, next) => {
-  if (req.session.User) {
-    res.redirect('/');
-  }
-
-  const username = req.body.username;
-  const password = req.body.password;
-
-  // Validate
-  if (username === '' || password === '' || password.length < 8 || !password.match(/[A-Z]/)) {
-    const data = {
-      title: 'Signup',
-      message: 'Try again'
-    };
-    return res.render('auth/signup', data);
-  }
-
-  // check if user with this username already exists
-  User.findOne({ 'username': username }, (err, user) => {
-    if (err) {
-      return next(err);
-    }
-    if (user) {
-      const data = {
-        title: 'Signup',
-        message: 'The "' + username + '" username is taken'
-      };
-      return res.render('auth/signup', data);
-    }
-    const salt = bcrypt.genSaltSync(bcryptSalt);
-    const hashPass = bcrypt.hashSync(password, salt);
-
-    const newUser = new User({
-      username,
-      password: hashPass
-    });
-
-    newUser.save((err) => {
-      if (err) {
-        return next(err);
-      }
-      req.session.currentUser = newUser;
-      res.redirect('/');
-    });
-  });
-});
-
-authRoutes.post('/logout', (req, res, next) => {
+routes.post('/logout', (req, res, next) => {
   req.session.currentUser = null;
   res.redirect('/');
 });
 
-module.exports = authRoutes;
+module.exports = routes;
