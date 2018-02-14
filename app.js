f6f9d07d0f4e4b13de574abc87d37092b0c0b710
Pere Amorós <amorospere@gmail.com>
2018-02-14 13:25:18 +0100

    Add ENV variables

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const logger = require('morgan');
@@ -17,7 +18,7 @@ const app = express();
 
 // DATABASE SETUP
 mongoose.Promise = Promise;
-mongoose.connect('mongodb://localhost/wyrcan-db', {
+mongoose.connect(process.env.MONGODB_URI, {
   keepAlive: true,
   reconnectTries: Number.MAX_VALUE
 });
